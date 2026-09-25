5c8359698576268d55ca4f63a61de498304fc1c8
Gabriel Vieira <nox030705@gmail.com>
2026-09-25 00:53:01 -0400

    feat(root): added persistent flags

--- internal/cli/root.go
+++ internal/cli/root.go
@@ -7,9 +7,19 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var Verbose bool
+var Quiet bool
+var DryRun bool
+var Yes bool
+
 var rootCmd = cobra.Command{
 	Use:   "dots",
 	Short: "A tool to manage dotfiles with symlinks",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		cmd.Help()
+
+		return nil
+	},
 }
 
 func Execute() {
@@ -17,4 +27,11 @@ func Execute() {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
+}
+
+func init() {
+	rootCmd.PersistentFlags().BoolVarP(&Verbose, "verbose", "v", false, "Verbose output")
+	rootCmd.PersistentFlags().BoolVar(&Quiet, "quiet", false, "Quiet output")
+	rootCmd.PersistentFlags().BoolVarP(&DryRun, "dry-run", "d", false, "Dry run")
+	rootCmd.PersistentFlags().BoolVar(&Yes, "yes", false, "Answer yes to all questions")
 }
